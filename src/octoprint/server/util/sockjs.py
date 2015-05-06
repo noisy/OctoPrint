noisy/OctoPrint

commit f19d0f0360ce102bd601fc571bb1c17878f136c4

Base cut off point for temperature graph on timestamps instead of data points
Cut off of the temperature graph is now not based on the number of data points any more but on the actual time of the data points. Anything older than ``n`` minutes will be cut off, with ``n`` defaulting to 30min. This value can be changed under "Temperatures" in the Settings

Closes #343

src/octoprint/static/js/app/viewmodels/temperature.js
@@ -32,6 +32,7 @@ $(function() {
         self.isLoading = ko.observable(undefined);
 
         self.temperature_profiles = self.settingsViewModel.temperature_profiles;
+        self.temperature_cutoff = self.settingsViewModel.temperature_cutoff;
 
         self.heaterOptions = ko.observable({});
 
@@ -161,11 +162,6 @@ $(function() {
             if (!CONFIG_TEMPERATURE_GRAPH) return;
 
             self.temperatures = self._processTemperatureData(data, self.temperatures);
-            _.each(_.keys(self.heaterOptions()), function(d) {
-                self.temperatures[d].actual = self.temperatures[d].actual.slice(-300);
-                self.temperatures[d].target = self.temperatures[d].target.slice(-300);
-            });
-
             self.updatePlot();
         };
 
@@ -213,6 +209,16 @@ $(function() {
 
                     self.hasBed(self.hasBed() || (type == "bed"));
                 })
+            });
+
+            var now = Date.now();
+            var filterOld = function(item) {
+                return item[0] >= now - self.temperature_cutoff() * 60 * 1000;
+            };
+
+            _.each(_.keys(self.heaterOptions()), function(d) {
+                result[d].actual = _.filter(result[d].actual, filterOld);
+                result[d].target = _.filter(result[d].target, filterOld);
             });
 
             return result;

src/octoprint/printer/standard.py
@@ -23,6 +23,7 @@ from octoprint.printer import PrinterInterface, PrinterCallback, UnknownScript
 from octoprint.printer.estimation import TimeEstimationHelper
 from octoprint.settings import settings
 from octoprint.util import comm as comm
+from octoprint.util import InvariantContainer
 
 
 class Printer(PrinterInterface, comm.MachineComPrintCallback):
@@ -46,7 +47,7 @@ class Printer(PrinterInterface, comm.MachineComPrintCallback):
 		self._bedTemp = None
 		self._targetTemp = None
 		self._targetBedTemp = None
-		self._temps = deque([], 300)
+		self._temps = TemperatureHistory(cutoff=settings().getInt(["temperature", "cutoff"])*60)
 		self._tempBacklog = []
 
 		self._latestMessage = None
@@ -937,3 +938,12 @@ class StateMonitor(object):
 		}
 
 
+class TemperatureHistory(InvariantContainer):
+	def __init__(self, cutoff=30 * 60):
+
+		def temperature_invariant(data):
+			data.sort(key=lambda x: x["time"])
+			now = int(time.time())
+			return [item for item in data if item["time"] >= now - cutoff]
+
+		InvariantContainer.__init__(self, guarantee_invariant=temperature_invariant)

src/octoprint/server/api/settings.py
@@ -89,7 +89,8 @@ def getSettings():
 			"watched": s.getBaseFolder("watched")
 		},
 		"temperature": {
-			"profiles": s.get(["temperature", "profiles"])
+			"profiles": s.get(["temperature", "profiles"]),
+			"cutoff": s.getInt(["temperature", "cutoff"])
 		},
 		"system": {
 			"actions": s.get(["system", "actions"]),
@@ -210,6 +211,7 @@ def setSettings():
 
 	if "temperature" in data.keys():
 		if "profiles" in data["temperature"].keys(): s.set(["temperature", "profiles"], data["temperature"]["profiles"])
+		if "cutoff" in data["temperature"].keys(): s.setInt(["temperature", "cutoff"], data["temperature"]["cutoff"])
 
 	if "terminalFilters" in data.keys():
 		s.set(["terminalFilters"], data["terminalFilters"])

src/octoprint/settings.py
@@ -157,7 +157,8 @@ default_settings = {
 		"profiles": [
 			{"name": "ABS", "extruder" : 210, "bed" : 100 },
 			{"name": "PLA", "extruder" : 180, "bed" : 60 }
-		]
+		],
+		"cutoff": 30
 	},
 	"printerProfiles": {
 		"default": None,

src/octoprint/static/js/app/viewmodels/settings.js
@@ -109,8 +109,7 @@ $(function() {
         self.scripts_gcode_afterPrinterConnected = ko.observable(undefined);
     
         self.temperature_profiles = ko.observableArray(undefined);
-
-        self.temperature_profiles = ko.observableArray(undefined);
+        self.temperature_cutoff = ko.observable(undefined);
 
         self.system_actions = ko.observableArray([]);
 
@@ -260,6 +259,7 @@ $(function() {
             self.scripts_gcode_afterPrinterConnected(response.scripts.gcode.afterPrinterConnected);
     
             self.temperature_profiles(response.temperature.profiles);
+            self.temperature_cutoff(response.temperature.cutoff);
 
             self.system_actions(response.system.actions);
 
@@ -334,7 +334,8 @@ $(function() {
                     "watched": self.folder_watched()
                 },
                 "temperature": {
-                    "profiles": self.temperature_profiles()
+                    "profiles": self.temperature_profiles(),
+                    "cutoff": self.temperature_cutoff()
                 },
                 "system": {
                     "actions": self.system_actions()

src/octoprint/templates/dialogs/settings/temperatures.jinja2
@@ -1,4 +1,16 @@
 <form class="form-horizontal">
+    <h3>{{ _('Temperature Graph') }}</h3>
+    <div class="control-group">
+        <label class="control-label">{{ _('Graph cutoff') }}</label>
+        <div class="controls">
+            <div class="input-append">
+                <input type="number" class="input-mini text-right" data-bind="value: temperature_cutoff">
+                <span class="add-on">min</span>
+            </div>
+            <span class="help-block">{{ _('Needs a restart of OctoPrint to become active.') }}</span>
+        </div>
+    </div>
+    <h3>{{ _('Temperature Presets') }}</h3>
     <div class="row-fluid">
         <div class="offset4 span3"><h4>{{ _('Extruder') }}</h4></div>
         <div class="span3"><h4>{{ _('Bed') }}</h4></div>

src/octoprint/util/__init__.py
@@ -649,3 +649,36 @@ class CountedEvent(object):
 				self._counter = self._max
 			self._event.set()
 			self._logger.debug("Set event")
+
+
+class InvariantContainer(object):
+	def __init__(self, initial_data=None, guarantee_invariant=None):
+		from collections import Iterable
+		from threading import RLock
+
+		if guarantee_invariant is None:
+			guarantee_invariant = lambda data: data
+
+		self._data = []
+		self._mutex = RLock()
+		self._invariant = guarantee_invariant
+
+		if initial_data is not None and isinstance(initial_data, Iterable):
+			for item in initial_data:
+				self.append(item)
+
+	def append(self, item):
+		with self._mutex:
+			self._data.append(item)
+			self._data = self._invariant(self._data)
+
+	def remove(self, item):
+		with self._mutex:
+			self._data.remove(item)
+			self._data = self._invariant(self._data)
+
+	def __len__(self):
+		return len(self._data)
+
+	def __iter__(self):
+		return self._data.__iter__()

commit dc0c0245ff7b3322bae2cf5b386a137d94993597

<a> - Allow different timestamps (zones or dates) between client and server while keeping the temperature graph working
 The server now sends the current server time with each temperature update as well as the initial temperature data, allowing the client to translate the timestamps contained in the temperature logs to its local timescale.

 Followup to commit f19d0f0360ce102bd601fc571bb1c17878f136c4 which caused a problem with large deviations between server and client time.

src/octoprint/server/util/sockjs.py
@@ -8,6 +8,7 @@ __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms
 import logging
 import threading
 import sockjs.tornado
+import time
 
 import octoprint.timelapse
 import octoprint.server
@@ -98,6 +99,7 @@ class PrinterStateConnection(sockjs.tornado.SockJSConnection, octoprint.printer.
 			busy_files.append(dict(origin=data["job"]["file"]["origin"], name=data["job"]["file"]["name"]))
 
 		data.update({
+			"serverTime": time.time(),
 			"temps": temperatures,
 			"logs": logs,
 			"messages": messages,
@@ -106,7 +108,9 @@ class PrinterStateConnection(sockjs.tornado.SockJSConnection, octoprint.printer.
 		self._emit("current", data)
 
 	def on_printer_send_initial_data(self, data):
-		self._emit("history", data)
+		data_to_send = dict(data)
+		data_to_send["serverTime"] = time.time()
+		self._emit("history", data_to_send)
 
 	def sendEvent(self, type, payload=None):
 		self._emit("event", {"type": type, "payload": payload})

src/octoprint/static/js/app/viewmodels/temperature.js
@@ -117,13 +117,13 @@ $(function() {
 
         self.fromCurrentData = function(data) {
             self._processStateData(data.state);
-            self._processTemperatureUpdateData(data.temps);
+            self._processTemperatureUpdateData(data.serverTime, data.temps);
             self._processOffsetData(data.offsets);
         };
 
         self.fromHistoryData = function(data) {
             self._processStateData(data.state);
-            self._processTemperatureHistoryData(data.temps);
+            self._processTemperatureHistoryData(data.serverTime, data.temps);
             self._processOffsetData(data.offsets);
         };
 
@@ -137,7 +137,7 @@ $(function() {
             self.isLoading(data.flags.loading);
         };
 
-        self._processTemperatureUpdateData = function(data) {
+        self._processTemperatureUpdateData = function(serverTime, data) {
             if (data.length == 0)
                 return;
 
@@ -161,12 +161,12 @@ $(function() {
 
             if (!CONFIG_TEMPERATURE_GRAPH) return;
 
-            self.temperatures = self._processTemperatureData(data, self.temperatures);
+            self.temperatures = self._processTemperatureData(serverTime, data, self.temperatures);
             self.updatePlot();
         };
 
-        self._processTemperatureHistoryData = function(data) {
-            self.temperatures = self._processTemperatureData(data);
+        self._processTemperatureHistoryData = function(serverTime, data) {
+            self.temperatures = self._processTemperatureData(serverTime, data);
             self.updatePlot();
         };
 
@@ -183,8 +183,9 @@ $(function() {
             }
         };
 
-        self._processTemperatureData = function(data, result) {
+        self._processTemperatureData = function(serverTime, data, result) {
             var types = _.keys(self.heaterOptions());
+            var clientTime = Date.now();
 
             // make sure result is properly initialized
             if (!result) {
@@ -201,7 +202,8 @@ $(function() {
 
             // convert data
             _.each(data, function(d) {
-                var time = d.time * 1000;
+                var timeDiff = (serverTime - d.time) * 1000;
+                var time = clientTime - timeDiff;
                 _.each(types, function(type) {
                     if (!d[type]) return;
                     result[type].actual.push([time, d[type].actual]);
@@ -211,9 +213,8 @@ $(function() {
                 })
             });
 
-            var now = Date.now();
             var filterOld = function(item) {
-                return item[0] >= now - self.temperature_cutoff() * 60 * 1000;
+                return item[0] >= clientTime - self.temperature_cutoff() * 60 * 1000;
             };
 
             _.each(_.keys(self.heaterOptions()), function(d) {